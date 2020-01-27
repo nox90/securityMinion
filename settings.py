4f2316b7743366b502a3c7b92ea99825ff30610b
ssejzer <ssejzer@techzer.com.ar>
2020-01-27 08:42:41 +0200

    help syntax

--- settings.py
+++ settings.py
@@ -22,25 +22,22 @@ S3_content_type = 'application/octet-stream'
 
 help_message = '''
 ▀▀ *Security Minion {version}. Secrets Manager for Slack.* ▀▀
-
+​
 Usage:
-
-*/{name} help*
-> Returns this message
-*/{name} ?*
-> Returns a list of secrets stored for this channel
-*/{name} ??*
-> Returns a detailed list of secrets stored for this channel
-*/{name} _secret-name_*
-> Returns the stored secret
-*/{name} _secret-name_=_secret-value_*
-> Saves or updates a secret-value as secret-name
-*/{name} file _secret-name.txt_*
-*/{name} _secret-name.txt_=+*
-> Saves or updates a file as secret-name.txt
-*/{name} delete _secret-name_*
-*/{name} _secret-name_=*
-> Forgets a secret
-
-* Secret names and values are case sensitive.
+​
+> *List all* secrets stored for this channel (?? for extra info):
+/{name} `?`
+/{name} `??`
+> *Return* a stored secret:
+/{name} `secret-name`
+> *Save* or *update* a secret-value as secret-name:
+/{name} `secret-name`=`secret-value`
+> Save or update a *file* as secret-name.txt:
+/{name} file `secret-name.txt`
+> *Forget* a stored secret:
+/{name} delete `secret-name`
+> *Help* (this message):
+/{name} `help`
+​
+_Secret names and values are case sensitive._
 '''.format(name=botName, version=botVersion)
